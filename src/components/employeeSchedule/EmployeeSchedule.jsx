cbadd9b5a9746e76b72b33fb04a19b16bb9d6275
danugarri <danugarri@hotmail.com>
2022-09-16 20:35:24 +0200

    creating table to show schedule

--- src/components/employeeSchedule/EmployeeSchedule.jsx
+++ src/components/employeeSchedule/EmployeeSchedule.jsx
@@ -4,6 +4,22 @@ export const EmployeeSchedule = ({ schedule }) => {
   return (
     <>
       <h1>{Object.entries(schedule).join(',')}</h1>
+      <table>
+        <thead>
+          <tr>
+            {Object.entries(schedule).map((day, index) => (
+              <td key={index}>{day[0]}</td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {Object.entries(schedule).map((day, index) => (
+              <td key={index}>{day[1]}</td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
