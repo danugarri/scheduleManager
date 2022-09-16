6988b726d76a392e06436cb933b69103731434eb
danugarri <danugarri@hotmail.com>
2022-09-16 02:10:49 +0200

    working first version to get the exaclty worked hours schedule depending on the ordinay working hours

--- src/App.js
+++ src/App.js
@@ -32,11 +32,15 @@ function App() {
  const maxOrdinaryHoursPerDay= 9
 //  Generate number of hours per day acording to rules
  let eachDay=[]
- for (let i=0 ; i<7; i++) {
-  eachDay=eachDay.concat(Math.floor(Math.random()* (maxOrdinaryHoursPerDay-minHoursPerDay)+minHoursPerDay)) 
+ const generateHours= () => {
+
+   for (let i=0 ; i<7; i++) {
+    eachDay=eachDay.concat(Math.floor(Math.random()* (maxOrdinaryHoursPerDay-minHoursPerDay)+minHoursPerDay)) 
+   }
  }
+ generateHours()
  console.log(eachDay)
- const schedule= {
+ let schedule= {
   monday:eachDay[0],
   tuesday:eachDay[1],
   wednesday:eachDay[2],
@@ -46,16 +50,36 @@ function App() {
   sunday:eachDay[6]
  }
  let  realWorkedHours=0
- for (let day in schedule) {
+ const manageDays= () =>{
+   for (let day in schedule) {
   // Set 0 hours if is freeday
  const isFreeDay= freeDays.find(freeDay=> freeDay === day )
  if (isFreeDay) {
   schedule[day]= 0
  }
   realWorkedHours+= schedule[day]
- }
+ }}
+ manageDays()
  console.log(realWorkedHours)
-
+while(realWorkedHours !== ordinaryEmployeeHours){
+console.log('is doing different hours than their contracy says')
+// console.log(realWorkedHours)
+eachDay=[]
+realWorkedHours=0
+generateHours()
+console.log(eachDay)
+schedule= {
+  monday:eachDay[0],
+  tuesday:eachDay[1],
+  wednesday:eachDay[2],
+  thursday:eachDay[3],
+  friday:eachDay[4],
+  saturday:eachDay[5],
+  sunday:eachDay[6]
+ }
+manageDays()
+console.log(realWorkedHours)
+}
   }
   scheduleManagement(mockOrdinaryEmployeeHours,mockFreeDays,mockWorkersPerTurn)
   return (
