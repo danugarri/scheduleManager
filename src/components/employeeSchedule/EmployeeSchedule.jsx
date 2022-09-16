b97a100a7ad5b74d4697c84b4747305cb42a2950
danugarri <danugarri@hotmail.com>
2022-09-16 20:28:44 +0200

    placed Modal at the root component

--- src/components/employeeSchedule/EmployeeSchedule.jsx
+++ src/components/employeeSchedule/EmployeeSchedule.jsx
@@ -3,11 +3,6 @@ import React from 'react';
 export const EmployeeSchedule = ({ schedule }) => {
   return (
     <>
-      {/* <h3>Example with 37h</h3>
-
-      <h1>{Object.entries(schedule).join(',')}</h1>
-      <h3>Example with 20h</h3> */}
-
       <h1>{Object.entries(schedule).join(',')}</h1>
     </>
   );
