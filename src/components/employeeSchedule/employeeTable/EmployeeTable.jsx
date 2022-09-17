208ee66c2580f034ac3a5db09394c5e2459df225
danugarri <danugarri@hotmail.com>
2022-09-17 14:01:21 +0200

    changed table className

--- src/components/employeeSchedule/employeeTable/EmployeeTable.jsx
+++ src/components/employeeSchedule/employeeTable/EmployeeTable.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 export const EmployeeTable = ({ schedule }) => {
   return (
     <>
-      <table className='schedule-table'>
+      <table className='employee-table'>
         <thead>
           <tr>
-            {/* Rendering if the object is defined with the table properties */}
             {Object.entries(schedule).map((day, index) => (
               <td key={index}>{day[0]}</td>
             ))}
