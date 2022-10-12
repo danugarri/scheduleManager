523b023a7b477680dcf9ce38d54d8958f06e9dc9
danugarri <danugarri@hotmail.com>
2022-10-12 16:25:57 +0200

    corrected checkMaxHours function

--- src/helpers/checkMaxHoursAccordingToFreeDays.js
+++ src/helpers/checkMaxHoursAccordingToFreeDays.js
@@ -1,21 +1,28 @@
 export const checkMaxHoursAccordingToFreeDays = (allDays, workingHoursPerDay, freeDays) => {
-  const maxHours = {};
+  let maxHours = {};
+  const freeDaysObject = {};
   let maxHoursToDo = 0;
-  for (const day in allDays) {
-    // If the subtraction is less tahn or equals 9 (the max hours per day)
-    if (workingHoursPerDay - allDays[day] >= 9) {
-      Object.assign(maxHours, { [day]: 9 });
-    }
-    freeDays.forEach((freeDay) => {
-      if ([day] === freeDay) {
-        Object.assign(maxHours, { [day]: 0 });
+  freeDays.forEach((freeDay) => {
+    for (const day in allDays) {
+      // If the subtraction is less tahn or equals 9 (the max hours per day)
+      if (workingHoursPerDay - allDays[day] >= 9) {
+        maxHours = { ...maxHours, [day]: 9 };
       }
-    });
-    Object.assign(maxHours, { [day]: workingHoursPerDay - allDays[day] });
-  }
+      if (workingHoursPerDay - allDays[day] < 9) {
+        maxHours = { ...maxHours, [day]: workingHoursPerDay - allDays[day] };
+      }
+
+      if (day === freeDay) {
+        Object.assign(freeDaysObject, { [day]: 0 });
+      }
+    }
+  });
+  Object.assign(maxHours, freeDaysObject);
+  console.log(maxHours);
   //   Return total sumation according to the previous
   for (const day in maxHours) {
     maxHoursToDo += maxHours[day];
   }
+  console.log(maxHoursToDo);
   return maxHoursToDo;
 };
