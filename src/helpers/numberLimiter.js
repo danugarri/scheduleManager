68e30f65c34bb4331f1618a701c97cf3509c30af
danugarri <danugarri@hotmail.com>
2022-10-24 00:33:31 +0200

    reversed

--- src/helpers/numberLimiter.js
+++ src/helpers/numberLimiter.js
@@ -1,7 +1,4 @@
 export const numberLimiter = (input) => {
   const hoursMaxLength = 2;
-  if (typeof input === 'string') {
-    input.replace(input, '');
-  }
   return input.slice(0, hoursMaxLength);
 };
