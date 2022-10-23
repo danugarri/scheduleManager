777081da4a692b22dcca27ec48dfaf5ffc2aa79d
danugarri <danugarri@hotmail.com>
2022-10-24 00:18:09 +0200

    added string input controller for ordinaryEmployeeHours

--- src/helpers/numberLimiter.js
+++ src/helpers/numberLimiter.js
@@ -1,5 +1,7 @@
 export const numberLimiter = (input) => {
   const hoursMaxLength = 2;
-
+  if (typeof input === 'string') {
+    input.replace(input, '');
+  }
   return input.slice(0, hoursMaxLength);
 };
