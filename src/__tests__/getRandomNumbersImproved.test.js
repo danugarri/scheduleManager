607d4786a4e17541be2a81965330ce23e60070e9
danugarri <danugarri@hotmail.com>
2022-11-13 14:47:29 +0100

    added unit test for getRandomSpecifiedWIthHalf function

--- src/__tests__/getRandomNumbersImproved.test.js
+++ src/__tests__/getRandomNumbersImproved.test.js
@@ -1,8 +1,8 @@
-import { getNew } from '../helpers/getRandomNumbersImproved';
+import { getNew, getRandomSpecifiedWIthHalf } from '../helpers/getRandomNumbersImproved';
 
-describe('the getting random numbers between 2 and 9', () => {
+describe('the getNew function', () => {
   const randomNumber = getNew(2, 9);
-  test('the getNew function', () => {
+  test('the getting random numbers between 2 and 9', () => {
     const expectedNumbers = randomNumber >= 2 && randomNumber < 10;
     expect(expectedNumbers).toBe(true);
   });
@@ -11,5 +11,16 @@ describe('the getting random numbers between 2 and 9', () => {
   });
   test('not to be 0 or 1', () => {
     expect(randomNumber).not.toBe(0 || 1);
+  });
+});
+describe('the getRandomSpecifiedWIthHalf function', () => {
+  const excludedNumbers = [0.5, 1, 1.5, 9.5];
+  const randomNumber = getRandomSpecifiedWIthHalf(excludedNumbers, 0, 9);
+  test('exclude specified numbers', () => {
+    expect(randomNumber).not.toBe(0.5 || 1 || 1.5 || 9.5);
+  });
+  test('get numbers between 0 and 9', () => {
+    const expectedNumbers = randomNumber >= 0 && randomNumber <= 9;
+    expect(expectedNumbers).toBe(true);
   });
 });
