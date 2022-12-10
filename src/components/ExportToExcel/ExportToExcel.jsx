d3d8bfdd70ba47119c304ee81ecaeef9b06ecb83
danugarri <danugarri@hotmail.com>
2022-12-10 21:07:05 +0100

    workaround to download the file

--- src/components/ExportToExcel/ExportToExcel.jsx
+++ src/components/ExportToExcel/ExportToExcel.jsx
@@ -17,7 +17,7 @@ export const ExportToExcel = ({ csvData }) => {
   const downloadFile = () => {
     if (formattedCsv.length > 0) {
       postData(formattedCsv).then((response) => {
-        window.open(response.data, '_self');
+        window.open(response.data);
       });
     }
   };
