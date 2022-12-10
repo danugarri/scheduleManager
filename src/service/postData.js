394909eca4f62738b197588d0e865bcaf9bacef5
danugarri <danugarri@hotmail.com>
2022-12-10 13:47:31 +0100

    updated url to send the request

--- src/service/postData.js
+++ src/service/postData.js
@@ -1,7 +1,9 @@
 // import { dataExample } from './consts';
 
 export const postData = async (csvData) => {
-  const response = await fetch('http://localhost:5000', {
+  // const initialUrl = 'https://excel-file-writer-improved.onrender.com';
+  const deployedUrl = 'https://excel-file-writer-improved.onrender.com';
+  const response = await fetch(deployedUrl, {
     body: JSON.stringify(csvData),
     method: 'POST',
     headers: {
