cc362429be99c111c8eb1541b9bc2b69a2dc1043
danugarri <danugarri@hotmail.com>
2022-12-13 15:59:52 +0100

    style

--- src/components/employeesView/EmployeesView.jsx
+++ src/components/employeesView/EmployeesView.jsx
@@ -5,9 +5,13 @@ export const EmployeesView = ({ data, openEmployees }) => {
   const employee = data.map((employee) => (
     <main className='employee-container'>
       <section className='employee-content-container'>
-        <p className='bold-text employee-name'>{` ${employee.employeeName}`}</p>
+        <section className='employee-header'>
+          <p className='bold-text employee-name'>{` ${employee.employeeName}`}</p>
+          <div className='employee-deletion'>X</div>
+        </section>
         <section className='color-container'>
           <p className='bold-text'>Color: </p>
+
           <div style={{ backgroundColor: employee.color }} className='employee-color-view'></div>
         </section>
       </section>
