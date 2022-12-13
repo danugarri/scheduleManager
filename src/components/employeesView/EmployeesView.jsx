672c3eceb751e2ea681534f32eae5a7ab0e6221e
danugarri <danugarri@hotmail.com>
2022-12-13 15:53:26 +0100

    style

--- src/components/employeesView/EmployeesView.jsx
+++ src/components/employeesView/EmployeesView.jsx
@@ -3,13 +3,15 @@ import './EmployeesView.css';
 
 export const EmployeesView = ({ data, openEmployees }) => {
   const employee = data.map((employee) => (
-    <section className='employee-container'>
-      <p className='bold-text'>{` ${employee.employeeName}`}</p>
-      <section className='color-container'>
-        <p className='bold-text'>Color: </p>
-        <div style={{ backgroundColor: employee.color }} className='employee-color-view'></div>
+    <main className='employee-container'>
+      <section className='employee-content-container'>
+        <p className='bold-text employee-name'>{` ${employee.employeeName}`}</p>
+        <section className='color-container'>
+          <p className='bold-text'>Color: </p>
+          <div style={{ backgroundColor: employee.color }} className='employee-color-view'></div>
+        </section>
       </section>
-    </section>
+    </main>
   ));
   return <>{openEmployees && employee}</>;
 };
