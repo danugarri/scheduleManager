fcaab57dd410155a5e660b01b3156aeb4a755a64
danugarri <danugarri@hotmail.com>
2022-12-19 13:51:32 +0100

    working deleting the correct employee

--- src/components/employeesView/EmployeesView.jsx
+++ src/components/employeesView/EmployeesView.jsx
@@ -8,28 +8,34 @@ export const EmployeesView = ({ data, openEmployees, setEmployeeRequest }) => {
   const [employeeDeleted, setEmployeeDeleted] = useState({});
   const [openDeletedModal, setOpenDeletedModal] = useState(false);
 
-  const sendDelete = () => {
-    const id = document.getElementById('employee-container').ariaValueText;
+  const sendDelete = (e) => {
+    const id = e.currentTarget.ariaValueText;
     deleteEmployee(id).then((response) => {
       const employeeDeleted = data.find((employee) => employee._id === response._id);
-      console.log(employeeDeleted.employeeName);
+      console.log(employeeDeleted._id);
       setEmployeeDeleted({ name: employeeDeleted.employeeName, message: 'deleted' });
       setOpenDeletedModal(true);
       setEmployeeRequest({ name: employeeDeleted.employeeName, color: '', message: 'deleted' });
     });
   };
+  const [hover, setHover] = useState(false);
 
-  const employee = data.map((employee) => (
-    <div
-      key={employee._id}
-      className='employee-container'
-      id='employee-container'
-      aria-valuetext={employee._id}
-    >
+  const myStyle = hover ? 'red' : '';
+
+  const employee = data.map((employee, index) => (
+    <div key={employee._id} className='employee-container' style={{ backgroundColor: myStyle }}>
       <section className='employee-content-container'>
         <section className='employee-header'>
           <p className='bold-text employee-name'>{` ${employee.employeeName}`}</p>
-          <div onClick={sendDelete} className='employee-deletion'>
+          <div
+            onMouseOver={(e) => {
+              setHover(true);
+            }}
+            onMouseOut={() => setHover(false)}
+            onClick={(e) => sendDelete(e)}
+            className='employee-deletion'
+            aria-valuetext={employee._id}
+          >
             X
           </div>
         </section>
